87dd28bd59f19b76ba467d884446d04d19c0f47a
kenarigame <kenarigame@gmail.com>
2026-06-29 14:01:42 +0700

    fist commit

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,13 @@
+
+function Homepage() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* <Navbar />
+      <Hero />
+      <Grid />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default Homepage;
